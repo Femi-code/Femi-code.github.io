dd0a1f0558bae0f46702f5a341163359b5fc9580
Femi-code <66394869+Femi-code@users.noreply.github.com>
2020-06-16 02:54:36 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+/* navigation scroll*/
+$(function(){
+
+$('a[href*="#"]:not([href=#])').click(function(){
+    if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')&& location.hostname==this.hostname){
+      var target=$(this.hash);
+      target=target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if(target.length){
+      	$('html,body').animate({
+      		scrollTop: target.offset().top
+      	}, 1000)
+      	return false;
+      }
+    }
+});
+
+
+});
+
+
+/* animation-on-scroll*/
+
+
+$('.js--wp-1').waypoint(function(direction){
+  $('.js--wp-1').addClass('animated fadeInUp');
+}, {
+  offset: '50%'
+
+});
+
+$('.js--wp-2').waypoint(function(direction){
+  $('.js--wp-2').addClass('animated fadeInUp');
+}, {
+  offset: '50%'
+
+});
+
+$('.js--wp-3').waypoint(function(direction){
+  $('.js--wp-3').addClass('animated fadeInUp');
+}, {
+  offset: '50%'
+
+
+});
+
+
+
+});
